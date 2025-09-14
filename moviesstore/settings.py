6ab20d21fc4b8657241334ad8a0371c47a41d316
Khayal Shah <khayalshah@Khayals-MacBook-Air.local>
2025-09-14 14:59:35 -0400

    Describe what you changed

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "khayalshah.pythonanywhere.com",
+    "khayalshah1.pythonanywhere.com",
 ]
 
 
--- moviesstore/wsgi.py
+++ moviesstore/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
+
+# +++++++++++ DJANGO +++++++++++
+# To use your own django app use code like this:
 import os
+import sys
 
+## assuming your django settings file is at '/home/khayalshah1/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/khayalshah1/mysite/manage.py'
+path = '/home/khayalshah1/moviesstore'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'moviesstore.settings'
+
+## then:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moviesstore.settings')
-
 application = get_wsgi_application()
